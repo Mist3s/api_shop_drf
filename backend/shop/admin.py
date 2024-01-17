dacdb3345d8226b5d780b91576e8a95c6b5abd7f
andrey <a@a.ru>
2024-01-17 15:19:39 +0200

    Регистрация новых моделей в админке.

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import Category, Product
+from .models import (
+    Category, Product, Packaging,
+    ProductPackaging, ProductImage
+)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3  # Количество дополнительных полей для загрузки изображений
+
+
+class PackagingInline(admin.TabularInline):
+    model = ProductPackaging
+    extra = 3  # Количество дополнительных полей для добавления упаковки
+
+
+@admin.display(description='Изображение')
+def display_image(obj):
+    return mark_safe(f'<img src="{obj.image.url}" height="60" />')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = (display_image, 'product')
 
 
 @admin.register(Product)
@@ -19,3 +43,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, PackagingInline]
+
+
+@admin.register(Packaging)
+class PackagingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'weight']
+
+
+

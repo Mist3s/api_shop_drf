face51bc514e4d12c0272a451569f1f7840d83e3
andrey <a@a.ru>
2024-02-14 15:59:23 +0200

    Разработать структуру модели User.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+REGEX_SIGNS = RegexValidator(r'^[\w.@+-]+\Z', 'Поддерживаемые символы.')
+REGEX_ME = RegexValidator(r'[^m][^e]', 'Имя пользователя не может быть "me".')
+
+
+class User(AbstractUser):
+    """Модель пользователей."""
+    username = models.CharField(
+        unique=True,
+        max_length=150,
+        validators=(REGEX_SIGNS, REGEX_ME),
+        verbose_name='Никнейм пользователя',
+        help_text='Укажите никнейм пользователя'
+    )
+    email = models.EmailField(
+        unique=True,
+        max_length=254,
+        verbose_name='E-mail пользователя',
+        help_text='Укажите e-mail пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        help_text='Укажите имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия пользователя',
+        help_text='Укажите фамилия пользователя'
+    )
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username

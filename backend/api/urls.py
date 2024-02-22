5ddc3d8848633e363c309a0637ceb7fd76688bcf
andrey <mist3s@github.com>
2024-02-22 18:39:46 +0200

    Убрать лишние переносы.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-
 v1_router = routers.DefaultRouter()
-
 
 urlpatterns = [
     path('api/', include('v1_router.urls', namespace='api_v1')),

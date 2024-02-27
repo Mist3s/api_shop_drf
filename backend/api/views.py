d72c2983ae254c70bb71e1516ff76aa3741c1873
andrey <a@a.ru>
2024-02-27 13:40:52 +0200

    Предварительный вариант вьюсет модели продукта.

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from .serializers import CategorySerializer
-from shop.models import Category
+from .serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для модели категории."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (AllowAny,)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     permission_classes = (AllowAny,)

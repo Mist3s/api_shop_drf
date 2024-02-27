0cad2c6fe03a9d74b6fb9b893ff272899f15a0f3
andrey <a@a.ru>
2024-02-27 13:40:40 +0200

    Предварительный вариант сериализатора модели продукта.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,53 @@
 from rest_framework import serializers
 
-from shop.models import Category
+from shop.models import Category, Product, ProductImage, Packaging, ProductPackaging
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'name', 'slug')
+        fields = ('name', 'slug')
+
+
+class PackagingSerializer(serializers.ModelSerializer):
+    # name = serializers.CharField(source='product_packaging.name')
+    # weight = serializers.FloatField(source='product_packaging.weight')
+    # price = serializers.FloatField(source='product_packaging.price')
+
+    class Meta:
+        model = Packaging
+        fields = (
+            'name',
+            'weight',
+            # 'price'
+        )
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ('id', 'image', 'preview')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    packaging = PackagingSerializer(
+        many=True
+    )
+    images = ProductImageSerializer(
+        source='product_images',
+        many=True
+    )
+
+    class Meta:
+        model = Product
+        fields = (
+            'available',
+            'name',
+            'slug',
+            'category',
+            'description',
+            'packaging',
+            'created',
+            'updated',
+            'images'
+        )

161fab2e0cca9cfb053a17038703f36c9bf1f30f
andrey <mist3s@github.com>
2024-03-02 10:23:17 +0200

    Добавить вьюсет для модели упаковки.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from .serializers import CategorySerializer, ProductSerializer
-from shop.models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer, PackingSerializer
+from shop.models import Category, Product, Packing
+
+
+class PackingViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для модели категории."""
+    queryset = Packing.objects.all()
+    serializer_class = PackingSerializer
+    permission_classes = (AllowAny,)
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):

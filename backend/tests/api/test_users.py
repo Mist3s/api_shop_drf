d8a2bc8ddc6608a7d962ea09ba7f0b6847865ede
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-07 15:00:02 +0200

    Написать тест регистрации пользователя.

--- backend/tests/api/test_users.py
+++ backend/tests/api/test_users.py
@@ -1,2 +1,19 @@
 import pytest
+from rest_framework.test import APIClient
 
+client = APIClient()
+
+
+@pytest.mark.django_db
+def test_register_user():
+    payload = dict(
+        username='test_user',
+        email='test_user@test.ru',
+        password='Tes!tUs!er321!',
+        first_name='Test',
+        last_name='User'
+
+    )
+    response = client.post('/api/users/', payload)
+    data = response.data
+    assert data['username'] == payload['username']

af6aeb4215c577970aee5e03599ccd24fcb3bef9
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-07 13:30:19 +0200

    Убрать устаревший код.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,5 +6,5 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'username', 'email', 'first_name', 'last_name'
+        'username', 'email', 'first_name', 'last_name'
     )

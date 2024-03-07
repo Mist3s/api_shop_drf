e50b40fc8c647973e65e8d25083cd44536c0b0b6
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-07 15:29:43 +0200

    Написать тест на получения токена.

--- backend/tests/api/test_users.py
+++ backend/tests/api/test_users.py
@@ -6,3 +6,10 @@ def test_register_user(user, client):
     response = client.post('/api/users/', user)
     data = response.data
     assert data['username'] == user['username']
+
+
+@pytest.mark.django_db
+def test_login_user(user, client):
+    client.post('/api/users/', user)
+    response = client.post('/api/token/login/', dict(email=user['email'], password=user['password']))
+    assert response.data.get('auth_token')

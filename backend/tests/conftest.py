1d48d0d12e25f3b0990bb0e2cfaa40aae5a5d308
andrey <mist3s@github.com>
2024-03-10 17:12:33 +0200

    Добавить фукстуры для модели категории, картинок, упаковки и продукта.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,13 +1,70 @@
 import pytest
 from rest_framework.test import APIClient
 
-user_data = dict(
-    username='username',
-    email='test_user@test.ru',
-    password='TestPassword1!',
-    first_name='Test',
-    last_name='User'
-)
+from shop.models import Packing, Category
+
+
+
+@pytest.fixture
+def user_data():
+    return dict(
+        username='username',
+        email='test_user@test.ru',
+        password='TestPassword1!',
+        first_name='Test',
+        last_name='User'
+    )
+
+
+@pytest.fixture
+def category_data():
+    return dict(
+        name='category',
+        slug='category'
+    )
+
+
+@pytest.fixture
+def image_data():
+    return dict(
+        image=(
+            'data:image/gif;base64,R0lGODlhBwAKAMIEAICAgJmZmbOzs/f39///////'
+            '/////////yH5BAEKAAcALAAAAAAHAAoAAAMWSDPUGoE5AaIj1M4qMW+ZFDYD1ClnAgA7'
+        ),
+        preview=True
+    )
+
+
+@pytest.fixture
+def packing_data():
+    return dict(
+        name='packing',
+        weight=1
+    )
+
+
+@pytest.fixture
+def create_packing():
+    packing = Packing.objects.create(name='packing_1', weight=1)
+    return packing
+
+
+@pytest.fixture
+def create_category():
+    category = Category.objects.create(name='category-1', slug='category-1')
+    return category
+
+
+@pytest.fixture
+def product_data(image_data, create_packing, create_category):
+    return dict(
+        category=create_category.pk,
+        name='Product',
+        slug='product',
+        description='Test product!',
+        packing=[dict(id=create_packing.pk, price=100),],
+        images=[image_data,]
+    )
 
 
 @pytest.fixture
@@ -16,18 +73,21 @@ def client():
 
 
 @pytest.fixture
-def user(client):
+def user(client, user_data):
     response = client.post('/api/users/', user_data)
+    print(response)
     return response
 
 
 @pytest.fixture
-def get_auth_token(client, user):
+def auth_client(client, user_data):
     response = client.post(
         '/api/token/login/',
         dict(
-            email=user['email'],
+            email=user_data['email'],
             password=user_data['password']
         )
     )
-    return response.data.get('auth_token')
+    token = response.data.get('auth_token')
+    client.credentials(Authorization=f'Token {token}')
+    return client

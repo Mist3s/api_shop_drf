fdbda38b27569c59d919b180c08cc4b65778c39c
andrey <mist3s@github.com>
2024-03-10 15:01:49 +0200

    Добавить проверку кода ответа.

--- backend/tests/api/test_users.py
+++ backend/tests/api/test_users.py
@@ -1,9 +1,11 @@
 import pytest
+from rest_framework import status
 
 
 @pytest.mark.django_db
 def test_register_user(user, client):
     response = client.post('/api/users/', user)
+    assert response.status_code == status.HTTP_201_CREATED
     data = response.data
     assert data['username'] == user['username']
 
@@ -11,5 +13,12 @@ def test_register_user(user, client):
 @pytest.mark.django_db
 def test_login_user(user, client):
     client.post('/api/users/', user)
-    response = client.post('/api/token/login/', dict(email=user['email'], password=user['password']))
+    response = client.post(
+        '/api/token/login/',
+        dict(
+            email=user['email'],
+            password=user['password']
+        )
+    )
+    assert response.status_code == status.HTTP_200_OK
     assert response.data.get('auth_token')

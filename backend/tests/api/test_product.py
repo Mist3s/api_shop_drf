7010f6e96bd8f3c014c6c4d569ff3e47dcbdbad4
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-10 19:39:38 +0200

    Добавить тест для модели категории, запрос листа и создания категории.

--- backend/tests/api/test_product.py
+++ backend/tests/api/test_product.py
@@ -5,7 +5,6 @@ from rest_framework import status
 @pytest.mark.django_db
 def test_add_product(auth_client, product_data):
     response = auth_client.post('/api/products/', product_data, format='json')
-    print(response.data)
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data.get('id') == product_data.get('id')
     assert response.data.get('name') == product_data.get('name')
@@ -16,3 +15,23 @@ def test_add_product(auth_client, product_data):
     assert response.data.get('created')
     assert response.data.get('updated')
     assert response.data.get('available') == True
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'method, expected_status, data',
+    (
+        ('post', status.HTTP_405_METHOD_NOT_ALLOWED, pytest.lazy_fixture('category_data')),
+        ('get', status.HTTP_200_OK, None)
+    ),
+)
+def test_add_category_and_list(method, auth_client, expected_status, data, create_category):
+    response = getattr(auth_client, method)(
+        '/api/categories/', data
+    )
+    assert response.status_code == expected_status
+    if response.request.get('REQUEST_METHOD') == 'GET':
+        assert isinstance(response.data, list)
+        assert response.data[0]['slug'] == create_category.slug
+        assert response.data[0]['name'] == create_category.name
+

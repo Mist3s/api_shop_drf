5047c2f41275b46d79f9bc051803372acbf0eb2c
andrey <mist3s@github.com>
2024-03-10 15:38:07 +0200

    Поправить фикстуры.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 from rest_framework.test import APIClient
 
+user_data = dict(
+    username='username',
+    email='test_user@test.ru',
+    password='TestPassword1!',
+    first_name='Test',
+    last_name='User'
+)
+
 
 @pytest.fixture
 def client():
@@ -8,12 +16,18 @@ def client():
 
 
 @pytest.fixture
-def user():
-    return dict(
-        username='test_user',
-        email='test_user@test.ru',
-        password='Tes!tUs!er321!',
-        first_name='Test',
-        last_name='User'
+def user(client):
+    response = client.post('/api/users/', user_data)
+    return response
 
+
+@pytest.fixture
+def get_auth_token(client, user):
+    response = client.post(
+        '/api/token/login/',
+        dict(
+            email=user['email'],
+            password=user_data['password']
+        )
     )
+    return response.data.get('auth_token')

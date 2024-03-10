6030ee6bcb3d370e99e7dececb76f614fe170e67
andrey <mist3s@github.com>
2024-03-10 17:13:15 +0200

    Добавить тест создание продукта.

---
+++ backend/tests/api/test_product.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_add_product(auth_client, product_data):
+    response = auth_client.post('/api/products/', product_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data.get('id') == product_data.get('id')
+    assert response.data.get('name') == product_data.get('name')
+    assert response.data.get('category') == product_data.get('category')
+    assert response.data.get('description') == product_data.get('description')
+    assert response.data.get('packing')
+    assert response.data.get('images')

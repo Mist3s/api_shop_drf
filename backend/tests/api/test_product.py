6c7bddd6ff7c84ec65e8e5773a597efced7b4cf3
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-10 17:19:40 +0200

    Дополнить тест создание продукта проверкой наличие полей created, updated и available.

--- backend/tests/api/test_product.py
+++ backend/tests/api/test_product.py
@@ -5,6 +5,7 @@ from rest_framework import status
 @pytest.mark.django_db
 def test_add_product(auth_client, product_data):
     response = auth_client.post('/api/products/', product_data, format='json')
+    print(response.data)
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data.get('id') == product_data.get('id')
     assert response.data.get('name') == product_data.get('name')
@@ -12,3 +13,6 @@ def test_add_product(auth_client, product_data):
     assert response.data.get('description') == product_data.get('description')
     assert response.data.get('packing')
     assert response.data.get('images')
+    assert response.data.get('created')
+    assert response.data.get('updated')
+    assert response.data.get('available') == True

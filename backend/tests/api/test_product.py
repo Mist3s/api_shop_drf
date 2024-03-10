0c097e5b0ee2110061277f89f0b5455ad8ba62e8
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-10 20:46:43 +0200

    Добавить пояснение для тестов категории.

--- backend/tests/api/test_product.py
+++ backend/tests/api/test_product.py
@@ -19,24 +19,27 @@ def test_add_product(auth_client, product_data):
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    'method, expected_status, data, detail',
+    'method, expected_status, data, detail, text',
     (
-        ('post', status.HTTP_405_METHOD_NOT_ALLOWED, pytest.lazy_fixture('category_data'), False),
-        ('get', status.HTTP_200_OK, None, False),
-        ('get', status.HTTP_200_OK, None, True),
-        ('put', status.HTTP_405_METHOD_NOT_ALLOWED, pytest.lazy_fixture('category_data'), True),
-        ('patch', status.HTTP_405_METHOD_NOT_ALLOWED, pytest.lazy_fixture('category_data'), True),
-        ('delete', status.HTTP_405_METHOD_NOT_ALLOWED, None, True)
+        ('post', status.HTTP_405_METHOD_NOT_ALLOWED,
+         pytest.lazy_fixture('category_data'), False, 'Create category: Enabled.'),
+        ('get', status.HTTP_200_OK, None, False, 'List categories: Disabled.'),
+        ('get', status.HTTP_200_OK, None, True, 'Detailed information about category: Disabled.'),
+        ('put', status.HTTP_405_METHOD_NOT_ALLOWED,
+         pytest.lazy_fixture('category_data'), True, 'Update information about category: Enabled.'),
+        ('patch', status.HTTP_405_METHOD_NOT_ALLOWED,
+         pytest.lazy_fixture('category_data'), True, 'Update partial category information: Enabled.'),
+        ('delete', status.HTTP_405_METHOD_NOT_ALLOWED, None, True, 'Delete category: Enabled.')
     ),
 )
-def test_category(method, auth_client, expected_status, data, create_category, detail):
+def test_category(method, auth_client, expected_status, data, create_category, detail, text):
     url = '/api/categories/'
     if detail:
         url += f'{create_category.slug}/'
     response = getattr(auth_client, method)('/api/categories/', data)
-    assert response.status_code == expected_status
+    assert response.status_code == expected_status, text
     if response.request.get('REQUEST_METHOD') == 'GET':
         assert isinstance(response.data, list)
-        assert response.data[0].get('slug') == create_category.slug
-        assert response.data[0].get('name') == create_category.name
+        assert response.data[0].get('slug') == create_category.slug, 'Missing field "slug"'
+        assert response.data[0].get('name') == create_category.name, 'Missing field "name"'
 

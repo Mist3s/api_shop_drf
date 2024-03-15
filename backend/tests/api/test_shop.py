6e568890b7a23e9545d9b6125085b840534155a4
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-15 09:56:57 +0200

    Добавить типизацию и прогнать через flake8.

--- backend/tests/api/test_shop.py
+++ backend/tests/api/test_shop.py
@@ -1,43 +1,51 @@
-from decimal import Decimal
-
 import pytest
 from rest_framework import status
+from rest_framework.test import APIClient
+
+from shop.models import Packing, Category
 
 
 @pytest.mark.django_db
-def test_add_product_auth_superuser_client(auth_superuser_client, product_data):
-    response = auth_superuser_client.post('/api/products/', product_data, format='json')
-    assert response.status_code == status.HTTP_201_CREATED, 'Expected status code 201.'
-    assert response.data.get('id') == product_data.get('id'), 'Missing field "id".'
-    assert response.data.get('name') == product_data.get('name'), 'Missing field "name".'
-    assert response.data.get('category') == product_data.get('category'), 'Missing field "category".'
+def test_add_product_auth_superuser_client(
+        auth_superuser_client: APIClient, product_data: dict
+) -> None:
+    response = auth_superuser_client.post(
+        '/api/products/', product_data, format='json'
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data.get('id') == product_data.get('id')
+    assert response.data.get('name') == product_data.get('name')
+    assert response.data.get('category') == product_data.get('category')
     assert (response.data.get('description')
-            == product_data.get('description'), 'Missing field "description".')
-    assert response.data.get('packing'), 'Missing field "packing".'
+            == product_data.get('description'))
+    assert response.data.get('packing')
     for packing in response.data.get('packing'):
-        assert packing.get('weight'), 'Missing field "weight" for "packing".'
-        assert isinstance(packing.get('weight'), int), 'The field must be integer.'
-        assert (packing.get('price') == product_data.get('packing')[0].get('price'),
-                'Missing field "price" for "packing".')
-        assert (isinstance(Decimal(packing.get('price')), Decimal),
-                'The data in the field cannot be converted Decimal.')
-        assert packing.get('name'), 'Missing field "name" for "packing".'
-    assert response.data.get('images'), 'Missing field "images".'
+        assert packing.get('weight')
+        assert isinstance(packing.get('weight'), int)
+        assert (packing.get('price')
+                == product_data.get('packing')[0].get('price'))
+        assert packing.get('name')
+    assert response.data.get('images')
     for image in response.data.get('images'):
-        assert image.get('image'), 'Missing field "image" for "image".'
-        assert image.get('preview'), 'Missing field "preview" for "image".'
-    assert response.data.get('created'), 'Missing field "created".'
-    assert response.data.get('updated'), 'Missing field "updated".'
+        assert image.get('image')
+        assert image.get('preview')
+    assert response.data.get('created')
+    assert response.data.get('updated')
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     'client',
-    (pytest.lazy_fixture('no_auth_client'), pytest.lazy_fixture('auth_client')),
+    (
+        pytest.lazy_fixture('no_auth_client'),
+        pytest.lazy_fixture('auth_client')
+    ),
 )
-def test_add_product_auth_client(client, product_data):
+def test_add_product_auth_client(
+        client: APIClient, product_data: dict[str, list[dict], int, bool]
+) -> None:
     response = client.post('/api/products/', product_data, format='json')
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED, 'Only available superuser.'
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 @pytest.mark.django_db
@@ -49,7 +57,10 @@ def test_add_product_auth_client(client, product_data):
             pytest.lazy_fixture('auth_superuser_client')
     ),
 )
-def test_categories_get_detail(client, create_category):
+def test_categories_get_detail(
+        client: APIClient, create_category: Category
+) -> None:
+    """Проверка детальной информации категории."""
     fields_quantity = 2
     url = f'/api/categories/{create_category.slug}/'
     response = client.get(url)
@@ -58,8 +69,8 @@ def test_categories_get_detail(client, create_category):
     assert len(response.data) == fields_quantity
     assert response.data.get('name') == create_category.name
     assert response.data.get('slug') == create_category.slug
-    assert type(response.data.get('slug')) == str
-    assert type(response.data.get('name')) == str
+    assert isinstance(response.data.get('slug'), str)
+    assert isinstance(response.data.get('name'), str)
 
 
 @pytest.mark.django_db
@@ -85,9 +96,10 @@ def test_categories_get_detail(client, create_category):
     ),
 )
 def test_categories_method_not_available(
-        client, method, expected_status, data, detail, create_category
-):
-    url = f'/api/categories/'
+        client: APIClient, method: str, expected_status: status,
+        data: dict, detail: bool, create_category: Category
+) -> None:
+    url = '/api/categories/'
     if detail:
         url += f'{create_category.slug}/'
     response = getattr(client, method)(url, data, format='json')
@@ -103,7 +115,9 @@ def test_categories_method_not_available(
             pytest.lazy_fixture('auth_superuser_client')
     ),
 )
-def test_packing_get_list(client, create_packing):
+def test_packing_get_list(
+        client: APIClient, create_packing: Packing
+) -> None:
     fields_objects = 1
     url = '/api/packing/'
     response = client.get(url)
@@ -121,7 +135,9 @@ def test_packing_get_list(client, create_packing):
             pytest.lazy_fixture('auth_superuser_client')
     ),
 )
-def test_packing_get_detail(client, create_packing):
+def test_packing_get_detail(
+        client: APIClient, create_packing: Packing
+) -> None:
     url = f'/api/packing/{create_packing.pk}/'
     response = client.get(url)
     assert response.status_code == status.HTTP_200_OK
@@ -129,9 +145,9 @@ def test_packing_get_detail(client, create_packing):
     assert response.data.get('name') == create_packing.name
     assert response.data.get('id') == create_packing.id
     assert response.data.get('weight') == create_packing.weight
-    assert type(response.data.get('weight')) == int
-    assert type(response.data.get('id')) == int
-    assert type(response.data.get('name')) == str
+    assert isinstance(response.data.get('weight'), int)
+    assert isinstance(response.data.get('id'), int)
+    assert isinstance(response.data.get('name'), str)
 
 
 @pytest.mark.django_db
@@ -157,11 +173,11 @@ def test_packing_get_detail(client, create_packing):
     ),
 )
 def test_packing_method_not_available(
-        client, method, expected_status, data, detail, create_packing
-):
-    url = f'/api/packing/'
+        client: Packing, method: str, expected_status: status,
+        data: dict, detail: bool, create_packing: Packing
+) -> None:
+    url = '/api/packing/'
     if detail:
         url += f'{create_packing.pk}/'
     response = getattr(client, method)(url, data, format='json')
     assert response.status_code == expected_status
-

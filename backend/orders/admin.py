84cea384cbb37ace82181165990c27f6352e43a8
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-15 10:45:18 +0200

    Привести код в соответствие PEP8.

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -4,12 +4,14 @@ from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
+    """Инлайн вывод позиций заказа."""
     model = OrderItem
     raw_id_fields = ['product']
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    """Регистрация модели заказа в админке."""
     list_display = [
         'id', 'first_name', 'last_name', 'email',
         'address', 'postal_code', 'city', 'paid',

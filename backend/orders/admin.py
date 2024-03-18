f3b8c1dfe00c3e5735b895c9cc13805fe55ba6c4
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-03-18 20:20:41 +0200

    Зарегистрировать модель адреса в админке.

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Address
 
 
 class OrderItemInline(admin.TabularInline):
     """Инлайн вывод позиций заказа."""
     model = OrderItem
     raw_id_fields = ['product']
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    """Регистрация модели адреса в админке."""
+    list_display = [
+        'id', 'region', 'city', 'street',
+        'house', 'room'
+    ]
 
 
 @admin.register(Order)
